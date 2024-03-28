fd4b3c83b5bcdb277251d81825a79dacd552892a
NghiaDV <n_dang@lab.allabout.co.jp>
2024-03-28 14:53:02 +0700

    Enable cookie

--- wp-config-multiple-site.php
+++ wp-config-multiple-site.php
@@ -9,4 +9,5 @@ define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
 define('FS_METHOD','direct');
+define('COOKIE_DOMAIN', $_SERVER['HTTP_HOST']);
 /* That's all, stop editing! Happy publishing. */
